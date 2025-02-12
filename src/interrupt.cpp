691011080a05b78de5d92088a4204d321aceb330
Rodrigo Gonçalves de Branco <rodrigo.g.branco@gmail.com>
2025-02-12 15:46:59 -0300

    some minor fixes

--- src/interrupt.cpp
+++ src/interrupt.cpp
@@ -10,8 +10,7 @@ void default_irq_handler(int isr_nr, __attribute__((unused)) int err_code) {
   printk("IRQ ISR %x\n", isr_nr);
 };
 
-void irq0_system_timer(__attribute__((unused)) int isr_nr,
-                       __attribute__((unused)) int err_code) {
+void irq0_system_timer(int isr_nr, __attribute__((unused)) int err_code) {
   pic_send_eoi(isr_nr);
   // printk("IRQ0 %d\n", teste++);
   //  delay(20);
